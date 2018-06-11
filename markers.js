30630301b284cd84e06c47c08e2db7968c191f60
chandumelam123 <39194860+chandumelam123@users.noreply.github.com>
2018-06-11 14:32:45 +0530

    Add files via upload

---
+++ markers.js
@@ -0,0 +1,68 @@
+var myLocations = [
+    {
+        title: 'Sri Venkata Rama',
+        lat: 16.802465,
+        lng: 81.528731,
+        type: 'Entertainment'
+    },
+    {
+        title: 'Sri Venkateswara Complex',
+        lat: 16.759668,
+        lng: 81.690988,
+        type: 'Entertainment'
+    },
+    {
+        title: 'Costal City Center',
+        lat: 16.546613,
+        lng: 81.517296,
+        type:  'Entertainment'
+    },
+    {
+        title: 'Geetha Annapurna',
+        lat: 16.517616,
+        lng: 81.718397,
+        type:  'Entertainment'
+    },
+    {
+        title: 'VIT',
+        lat: 16.565965,
+        lng: 81.522512,
+        type: 'College'
+    },
+    {
+        title: 'Ananda',
+        lat: 16.543552,
+        lng: 81.513132,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Spice Magic',
+        lat: 16.545479,
+        lng: 81.520129,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Abhiruchi',
+        lat: 16.544542,
+        lng: 81.516544,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Kanaka Durga',
+        lat: 16.441179,
+        lng: 81.705397,
+        type: 'Entertainment'
+    },
+    {
+        title: 'Reliance ',
+        lat: 16.545208,
+        lng: 81.525220,
+        type: 'Shopping'
+    },
+    {
+        title: 'Chandrika',
+        lat: 16.543690,
+        lng: 81.516266,
+        type: 'Restaurant'
+    }
+]
---
+++ styles.js
@@ -0,0 +1,11 @@
+var styles = [
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e3eed3"
+            }
+        ]
+    }
+];
